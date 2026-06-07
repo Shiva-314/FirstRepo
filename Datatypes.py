6d27695f663637439f6c6e264da08c622fba88bd
Shiva-314 <worldaintelegence@gmsil.com>
2026-06-07 12:18:30 +0530

    Suraj G Solve the error

--- Datatypes.py
+++ Datatypes.py
@@ -4,7 +4,13 @@
 # U2 = int(input("Enter num2: "))
 
 
-U = int(input("Enter Sq to get Squrt: "))
-for i in range(2, U):
-    if (U%i == 0 and U // i == i):
-        print(f"Hence the Sqrt of {U} is: {i}")
+#  U = int(input("Enter Sq to get Squrt: "))
+#  for i in range(2, U):
+#      if (U%i == 0 and U // i == i):
+#          print(f"Hence the Sqrt of {U} is: {i}")
+
+X = int(input("Enter the num you want sqrt: "))
+n = 0
+while(n+1)*(n+1)<=X:
+    n+=1
+print(n)
