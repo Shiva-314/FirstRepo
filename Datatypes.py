e0fe7d57ee75747c1ca66ca8ecc74c34cd5a1de0
Shiva-314 <worldaintelegence@gmsil.com>
2026-06-07 12:28:31 +0530

    Suraj G correct this Code

--- Datatypes.py
+++ Datatypes.py
@@ -1,15 +1,5 @@
 # print("Hello Shiva Sir !")
-
-# U = int(input("Enter num1: "))
-# U2 = int(input("Enter num2: "))
-
-
-#  U = int(input("Enter Sq to get Squrt: "))
-#  for i in range(2, U):
-#      if (U%i == 0 and U // i == i):
-#          print(f"Hence the Sqrt of {U} is: {i}")
-
-X = int(input("Enter the num you want sqrt: "))
+X = int(input("Enter num: "))
 n = 0
 while(n+1)*(n+1)<=X:
     n+=1
