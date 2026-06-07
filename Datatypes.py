fbaaced3cc0a7f983b1c2a2f45feea3bd5c3c3bc
Shiva-314 <worldaintelegence@gmsil.com>
2026-06-07 09:34:16 +0530

    Added a programe to find Sqrt

--- Datatypes.py
+++ Datatypes.py
@@ -1,4 +1,10 @@
-print("Hello Shiva Sir !")
+# print("Hello Shiva Sir !")
 
-U = int(input("Enter num1: "))
-U2 = int(input("Enter num2: "))
+# U = int(input("Enter num1: "))
+# U2 = int(input("Enter num2: "))
+
+
+U = int(input("Enter Sq to get Squrt: "))
+for i in range(2, U):
+    if (U%i == 0 and U // i == i):
+        print(f"Hence the Sqrt of {U} is: {i}")
